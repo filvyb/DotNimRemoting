99fc79b04a477bcf4b6182c2fc7ae8c06bbaa617
Filip Vybihal <101115161+filvyb@users.noreply.github.com>
2025-03-10 13:44:10 +0100

    Fix grammar test

---
+++ example.nim
@@ -0,0 +1,24 @@
+import src/msnrbf/grammar
+import src/msnrbf/helpers
+import src/msnrbf/enums
+import src/msnrbf/types
+import src/msnrbf/records/methodinv
+import src/msnrbf/records/member
+import options
+
+let methodName = newStringValueWithCode("Foo")
+let typeName = newStringValueWithCode("Bar")
+let binaryMethodCall = BinaryMethodCall(
+      recordType: rtMethodCall,
+      messageEnum: {ArgsInArray, NoContext},
+      methodName: methodName,
+      typeName: typeName
+    )
+let value = RemotingValue(
+      kind: rvPrimitive,
+      primitiveVal: PrimitiveValue(kind: ptInt32, int32Val: 10)
+    )
+let ctx = newSerializationContext()
+var msg = newRemotingMessage(ctx, methodCall = some(binaryMethodCall), callArray = @[value])
+var serialized = serializeRemotingMessage(msg, ctx)
+echo serialized
